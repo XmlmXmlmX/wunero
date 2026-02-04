fa3a57bcb47db1a81c82755257bd4d66d19d0690
Sebastian Hösl <sebastian.hoesl@outlook.de>
2026-02-04 07:48:36 +0100

    feat: füge Unterstützung für Remote-Bilder von Gravatar hinzu

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,20 @@ const nextConfig: NextConfig = {
     // Types are validated locally before commit
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        pathname: '/avatar/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gravatar.com',
+        pathname: '/avatar/**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
